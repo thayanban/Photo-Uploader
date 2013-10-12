0e64dddaa8bf15207a124a43e7081cd11397b9aa
thayanban <thayanban@gmail.com>
2013-10-12 17:25:46 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 var express = require('express')
 ;
-var server = express() 
+var app = express() 
 ;
 var fs = require('fs')
 ;
 
-server.get('/home.html', function(req,res) {
+app.get('/home.html', function(req,res) {
 	res.setHeader('Content-Type' , 'text/html');
 	var stream = fs.createReadStream(__dirname + "/public/home.html");
 	stream.pipe(res);
 });
-server.get('/newalbum.html', function(req,res) {
+app.get('/newalbum.html', function(req,res) {
 	res.setHeader('Content-Type' , 'text/html');
 	var stream = fs.createReadStream(__dirname + "/public/newalbum.html");
 	stream.pipe(res);
 });
-server.get('/login.html', function(req,res) {
+app.get('/login.html', function(req,res) {
 	res.setHeader('Content-Type' , 'text/html');
 	var stream = fs.createReadStream(__dirname + "/public/login.html");
 	stream.pipe(res);
 });
-server.get('/register.html', function(req,res) {
+app.get('/register.html', function(req,res) {
 	res.setHeader('Content-Type' , 'text/html');
 	var stream = fs.createReadStream(__dirname + "/public/register.html");
 	stream.pipe(res);
 });
-server.listen(3000);
+app.listen(3000);
