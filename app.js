7d84e0837394432f5bee691028969d0bf18ea0d3
thayanban <thayanban@gmail.com>
2013-10-14 13:04:46 +0530

    updated package.json

--- app.js
+++ app.js
@@ -5,24 +5,33 @@ var app = express()
 var fs = require('fs')
 ;
 
-app.get('/home.html', function(req,res) {
+app.use(express.bodyParser());
+app.get('/home.html', function(req, res) {
 	res.setHeader('Content-Type' , 'text/html');
 	var stream = fs.createReadStream(__dirname + "/public/home.html");
 	stream.pipe(res);
 });
-app.get('/newalbum.html', function(req,res) {
+app.get('/newalbum.html', function(req, res) {
 	res.setHeader('Content-Type' , 'text/html');
 	var stream = fs.createReadStream(__dirname + "/public/newalbum.html");
 	stream.pipe(res);
 });
-app.get('/login.html', function(req,res) {
+app.get('/login.html', function(req, res) {
 	res.setHeader('Content-Type' , 'text/html');
 	var stream = fs.createReadStream(__dirname + "/public/login.html");
 	stream.pipe(res);
 });
-app.get('/register.html', function(req,res) {
+app.get('/register.html', function(req, res) {
 	res.setHeader('Content-Type' , 'text/html');
 	var stream = fs.createReadStream(__dirname + "/public/register.html");
 	stream.pipe(res);
+});
+app.post('/newalbum', function(req, res){
+    var image = req.body
+    , albumName = image.alname
+    , imageName = image.fname
+    ;
+    
+
 });
 app.listen(3000);
